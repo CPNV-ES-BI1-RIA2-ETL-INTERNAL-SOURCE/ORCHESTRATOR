8a16464764ac8c0be7a4abeceb617a0005ed10d5
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2025-01-22 10:37:24 +0100

    feat : create main endpoint

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from app.Orchestrator import Orchestrator
+from app.ConfigLoader import ConfigLoader
+from app.WorkflowManager import WorkflowManager
+from app.Caller import Caller
+from app.handlers.ErrorHandler import ErrorHandler
+
+from typing import List
+
+from app.models.RequestModel import RequestModel
 
 app = FastAPI()
 
+config_loader = ConfigLoader()
+caller = Caller()
+workflow_manager = WorkflowManager(config_loader.load_config('config/config.yaml'))
+error_handler = ErrorHandler()
+orchestrator = Orchestrator(workflow_manager, caller, config_loader)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
+@app.post("/start-process", response_model=RequestModel)
+def start_process(data: RequestModel):
+    try:
+        return orchestrator.start_process(data)
+    except Exception as e:
+        error_handler.log_error(str(e))
+        raise HTTPException(status_code=500, detail=str(e))
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
